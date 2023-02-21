aed4b558a936e1b35935c18689bc2cc23a9f4aef
imdamiandev <imdamian.dev@gmail.com>
2023-02-21 12:56:13 -0300

    layout mobile app

--- src/Calculator.js
+++ src/Calculator.js
@@ -41,26 +41,26 @@ function Calculator() {
   return (
     <div className="calculator">
       <input type="text" value={input} onChange={handleInput} />
-      <button onClick={handleClear}>C</button>
-      <button onClick={handleDelete}>DE</button>
-      <button onClick={handlePercent}>%</button>
-      <button onClick={() => handleButtonClick('/')}>/</button>
+      <button onClick={handleClear} className="operator-button">C</button>
+      <button onClick={handleDelete} className="operator-button">DE</button>
+      <button onClick={handlePercent} className="operator-button">%</button>
+      <button onClick={() => handleButtonClick('/')} className="operator-button">/</button>
       <button onClick={() => handleButtonClick('7')}>7</button>
       <button onClick={() => handleButtonClick('8')}>8</button>
       <button onClick={() => handleButtonClick('9')}>9</button>
-      <button onClick={() => handleButtonClick('*')}>x</button>
+      <button onClick={() => handleButtonClick('*')} className="operator-button">x</button>
       <button onClick={() => handleButtonClick('4')}>4</button>
       <button onClick={() => handleButtonClick('5')}>5</button>
       <button onClick={() => handleButtonClick('6')}>6</button>
-      <button onClick={() => handleButtonClick('-')}>-</button>
+      <button onClick={() => handleButtonClick('-')} className="operator-button">-</button>
       <button onClick={() => handleButtonClick('1')}>1</button>
       <button onClick={() => handleButtonClick('2')}>2</button>
       <button onClick={() => handleButtonClick('3')}>3</button>
-      <button onClick={() => handleButtonClick('+')}>+</button>
+      <button onClick={() => handleButtonClick('+')} className="operator-button">+</button>
       <button onClick={() => handleButtonClick('.')}>.</button>
       <button onClick={() => handleButtonClick('0')}>0</button>
       <button onClick={() => handleButtonClick('00')}>00</button>
-      <button onClick={handleEquals}>=</button>      
+      <button onClick={handleEquals} className="operator-button">=</button>      
     </div>
   );
 }
